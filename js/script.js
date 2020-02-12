63892c43d071723c48e41c20149feeacb470542f
Folafunmi <folafunmimustapha@gmail.com>
2020-02-13 00:06:35 +0100

    Update js file

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
 //write and and make a note to refactor this code 
+//freeCodeCamp Simon Game used to learn js
+
 let order = [];
 let playerOrder = [];
+let flash;
+let turn;
+let good;
+let compTurn; //who's turn it is to play
+let intervalId;
+let strict; //for if the check mark has been checked
+let noise = true;
+let on = false; //turning the game on or off but is initially off
+let win; //if the player has won the game
