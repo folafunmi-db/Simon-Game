cb85412c3137f561f908ca9f76142cda89bdf1d9
Folafunmi <folafunmimustapha@gmail.com>
2020-02-13 00:07:05 +0100

    Update js file

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ let turn;
 let good;
 let compTurn; //who's turn it is to play
 let intervalId;
-let strict; //for if the check mark has been checked
+let strict = false; //for if the check mark has been checked
 let noise = true;
 let on = false; //turning the game on or off but is initially off
 let win; //if the player has won the game
