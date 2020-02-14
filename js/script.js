a2cb372d39eb73b3385b25c77f4bfd9556d4607d
Folafunmi <folafunmimustapha@gmail.com>
2020-02-14 23:52:52 +0100

    Updated JS file

--- js/script.js
+++ js/script.js
@@ -23,20 +23,70 @@ const onButton = document.querySelector("#on");
 const startButton = document.querySelector("#start");
 
 //stating an arrow function in js
-strictButton.addEventListener("click", (event) => {
+strictButton.addEventListener("click", event => {
   if (strictButton.checked == true) {
     strict = ture;
   } else {
     strict = false;
   }
-})
+});
 
-onButton.addEventListener('click', (event) => {
-	if (onButton.checked == true) {
-		on = true;
-		turnCounter.innerHTML = "-";
-	} else {
-		on = false;
-		turnCounter.innerHTML = "";
+onButton.addEventListener("click", event => {
+  if (onButton.checked == true) {
+    on = true;
+    turnCounter.innerHTML = "-";
+  } else {
+    on = false;
+    turnCounter.innerHTML = "";
+    clearColor(); //all colours clear when the game is switched off
+    clearInterval(intervalId);
+  }
+});
+
+//the arrow function methos of defining functions was introduced in es6
+startButton.addEventListener("click", event => {
+  if (on || win) {
+    play();
+  }
+});
+
+//former way to define functions
+fucntion play() {
+	win = false;
+	order = []; //order in which the sectors will light up on the game
+	playerOrder = [];
+	flash = 0;
+	intervalId = 0;
+	turn = 1;
+	turnCounter.innerHTML = 1;
+	good = true;
+
+	for (var i = 0; i<20; i++) {
+		order.push(Math.floor(Math.random() * 4) * 1);
 	}
-})
+	compTurn = true;
+
+	intervalId = setInterval(gameTurn, 800);
+}
+
+function gameTurn() {
+	on = false;
+
+	if (flash == turn) {
+	//if the computer is done flashing all the lights
+	clearInterval(intervalId);
+	compTurn = false;
+	clearColor;
+	on = true;
+	}
+
+	if (compTurn0) {
+		clearCorol();
+		setTimeout(() => {
+			if (order(flash) == 1) one();
+			if (order(flash) == 1) one();
+			if (order(flash) == 1) one();
+			if (order(flash) == 1) one();
+		}, 200);
+	}
+}
