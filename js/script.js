2e2a22a665103e5f9d5563405f42865ca0ebfdd6
Folafunmi <folafunmimustapha@gmail.com>
2020-02-17 20:34:53 +0100

    Fixed the strictButton addEventListener

--- js/script.js
+++ js/script.js
@@ -25,7 +25,7 @@ const startButton = document.querySelector("#start");
 //stating an arrow function in js
 strictButton.addEventListener("click", event => {
   if (strictButton.checked == true) {
-    strict = ture;
+    strict = true;
   } else {
     strict = false;
   }
@@ -98,7 +98,7 @@ function one() {
 		audio.play();
 	}
 	noise = true;
-	topLeft.style.backgroundColor = "Lightgreeen";
+	topLeft.style.backgroundColor = "Lightgreen";
 }
 
 function two() {
@@ -107,7 +107,7 @@ function two() {
 		audio.play();
 	}
 	noise = true;
-	topLeft.style.backgroundColor = "tomato";
+	topRight.style.backgroundColor = "tomato";
 }
 
 function three() {
@@ -116,7 +116,7 @@ function three() {
 		audio.play();
 	}
 	noise = true;
-	topLeft.style.backgroundColor = "Yellow";
+	bottomLeft.style.backgroundColor = "Yellow";
 }
 
 function four() {
@@ -125,5 +125,117 @@ function four() {
 		audio.play();
 	}
 	noise = true;
-	topLeft.style.backgroundColor = "Lightskyblue";
+	bottomRight.style.backgroundColor = "Lightskyblue";
+}
+
+function clearColor() {
+	topLeft.style.backgroundColor = "darkgreen";
+	topRight.style.backgroundColor = "darkred";
+	bottomLeft.style.backgroundColor = "goldenrod";
+	bottomRight.style.backgroundColor = "darkblue";
+}
+
+function flashColor() {
+	topLeft.style.backgroundColor = "lightgreen";
+	topRight.style.backgroundColor = "tomato";
+	bottomLeft.style.backgroundColor = "yellow";
+	bottomRight.style.backgroundColor = "lightskyblue";
+}
+
+topLeft.addEventListener("click", (event) => {
+	if (on) {
+		playerOrder.push(1);
+		check();
+		one();
+		if(!win) {
+			setTimeout(() => {
+				clearColor();
+			}, 300);
+		}
+	}
+})
+
+topRight.addEventListener("click", (event) => {
+	if (on) {
+		playerOrder.push(2);
+		check();
+		two();
+		if(!win) {
+			setTimeout(() => {
+				clearColor();
+			}, 300);
+		}
+	}
+})
+
+bottomLeft.addEventListener("click", (event) => {
+	if (on) {
+		playerOrder.push(3);
+		check();
+		three();
+		if(!win) {
+			setTimeout(() => {
+				clearColor();
+			}, 300);
+		}
+	}
+})
+
+bottomRight.addEventListener("click", (event) => {
+	if (on) {
+		playerOrder.push(4);
+		check();
+		four();
+		if(!win) {
+			setTimeout(() => {
+				clearColor();
+			}, 300);
+		}
+	}
+})
+
+function check() {
+	if (playerOrder[playerOrder.length -1] !== order[playerOrder.length -1])
+	good = false;
+
+	if(playerOrder.length == 20 && good) {
+		winGame();
+	}
+
+	if(good == false) {
+		flashColor();
+		turnCounter.innerHTML = "NO!";
+		setTimeout(() => {
+			turnCounter.innerHTML = turn;
+			clearColor();
+
+			if (strict) {
+				play();
+			} else {
+				compTurn = true;
+				flash = 0;
+				playerOrder = [];
+				good = true;
+				intervalId = setInterval(gameTurn, 800);
+			}
+		}, 800);
+
+		noise = false;
+	}
+
+	if (turn == playerOrder.length && good && !win) {
+		turn++;
+		playerOrder = [];
+		compTurn = true;
+		flash = 0;
+		turnCounter.innerHTML = turn;
+		intervalId = setInterval(gameTurn, 800);
+	}
+}
+
+function winGame() {
+	flashColor();
+	turnCounter.innerHTML = "WIN!";
+	on = false;
+	win = true;
 }
